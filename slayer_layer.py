ce06009edebc727b9ba2d2a596c6609f29ab75a8
Felix Bauer <felix.bauer@aictx.ai>
2022-01-23 19:46:53 +0100

    Add alpha kernel to slayer that matches exp-leak + lif of exodus

--- slayer_layer.py
+++ slayer_layer.py
@@ -8,14 +8,17 @@ class SlayerLayer(slayerSNN.layer):
     def calculateSrmKernel(self):
         if self.neuron["type"] == "SRMALPHA":
             return super().calculateSrmKernel()
+        if self.neuron["type"] == "CUBALIF":
+            tau = self.neuron["tauSr"]
+            return self._calculateAlphaKernel(tau=tau, mult=1.0 / tau)
         elif self.neuron["type"] == "LIF":
             tau = self.neuron["tauSr"]
             return self._calculateLIFKernel(tau=tau, mult=1.0 / tau)
 
     def calculateRefKernel(self):
         if self.neuron["type"] == "SRMALPHA":
-            return super().calculateSrmKernel()
-        elif self.neuron["type"] == "LIF":
+            return super().calculateRefKernel()
+        elif self.neuron["type"] in ("CUBALIF", "LIF"):
             mult = -self.neuron["scaleRef"] * self.neuron["theta"]
             return self._calculateLIFKernel(tau=self.neuron["tauSr"], mult=mult)
 
@@ -27,3 +30,17 @@ class SlayerLayer(slayerSNN.layer):
                 break
 
         return torch.FloatTensor(kernel)
+
+    def _calculateAlphaKernel(self, tau, mult=1, EPSILON=1e-5):
+        # - Preserve original behavior for SRMALPHA
+        if self.neuron["type"] == "SRMALPHA":
+            return super()._calculateAlphaKernel(tau, mult, EPSILON)
+
+        # - This is similar to original alpha kernel, but shifted by one timestep and with different scaling
+        eps = []
+        for t in np.arange(0, self.simulation["tSample"], self.simulation["Ts"]):
+            epsVal = mult * (t + 1) * math.exp(-t / tau)
+            if abs(epsVal) < EPSILON and t > tau:
+                break
+            eps.append(epsVal)
+        return torch.FloatTensor(eps)
