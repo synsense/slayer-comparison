b715f79552f9a243ef0bcc6a1f82ba239d3e06c6
Felix Bauer <felix.bauer@aictx.ai>
2022-01-25 16:48:19 +0100

    enable setting run name for logs

--- train_nmnist.py
+++ train_nmnist.py
@@ -8,16 +8,23 @@ if __name__ == "__main__":
     pl.seed_everything(123)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--method", help="Can be 'sinabs' or 'exodus'.", type=str, default="exodus")
-    parser.add_argument("--architecture", help="Can be 'paper' or 'larger'.", type=str, default="paper")
+    parser.add_argument(
+        "--method", help="Can be 'sinabs' or 'exodus'.", type=str, default="exodus"
+    )
+    parser.add_argument(
+        "--architecture", help="Can be 'paper' or 'larger'.", type=str, default="paper"
+    )
     parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--dataset_fraction", type=float, default=1.)
-    parser.add_argument("--first_saccade_only", dest="first_saccade_only", action="store_true")
+    parser.add_argument("--dataset_fraction", type=float, default=1.0)
+    parser.add_argument(
+        "--first_saccade_only", dest="first_saccade_only", action="store_true"
+    )
     parser.add_argument("--augmentation", dest="augmentation", action="store_true")
     parser.add_argument("--n_time_bins", type=int, default=300)
     parser.add_argument("--tau_mem", type=float, default=20.0)
     parser.add_argument("--spike_threshold", type=float, default=0.1)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
+    parser.add_argument("--run_name", type=str, default="default")
     parser.set_defaults(first_saccade_only=False, augmentation=False)
     parser = pl.Trainer.add_argparse_args(parser)
     args = parser.parse_args()
@@ -40,7 +47,7 @@ if __name__ == "__main__":
         cache_dir="./cache/NMNIST/",
         fraction=args.dataset_fraction,
         augmentation=args.augmentation,
-   )
+    )
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         monitor="valid_loss",
@@ -50,11 +57,9 @@ if __name__ == "__main__":
         mode="min",
     )
 
+    logger = pl.loggers.TensorBoardLogger(save_dir="lightning_logs", name=args.run_name)
     trainer = pl.Trainer.from_argparse_args(
-        args,
-        logger=True,
-        callbacks=[checkpoint_callback],
-        log_every_n_steps=10,
+        args, logger=logger, callbacks=[checkpoint_callback], log_every_n_steps=10
     )
 
     trainer.logger.log_hyperparams(model.hparams)
