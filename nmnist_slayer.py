fix weight scaling for nmnist slayer

nmnist_slayer.py
@@ -1,24 +1,19 @@
 import torch
-import torch.nn as nn
 from torch.nn import functional as F
 import pytorch_lightning as pl
-import sinabs.layers as sl
-import sinabs.activation as sa
 from slayer_layer import SlayerLayer
-from typing import Dict, Any
 
 
 class SlayerNetwork(pl.LightningModule):
     def __init__(
         self,
-        batch_size=None,
         tau_mem=10.0,
         spike_threshold=0.1,
         learning_rate=1e-3,
         weight_decay=0,
         width_grad=1.0,
         scale_grad=1.0,
-        num_timesteps=100,
+        n_time_bins=100,
         architecture="paper",
     ):
         super().__init__()
@@ -33,7 +28,7 @@ class SlayerNetwork(pl.LightningModule):
             "tauRho": width_grad,
             "scaleRho": scale_grad,
         }
-        sim_params = {"Ts": 1.0, "tSample": num_timesteps}
+        sim_params = {"Ts": 1.0, "tSample": n_time_bins}
 
         self.slayer = SlayerLayer(neuron_params, sim_params)
 
@@ -41,11 +36,11 @@ class SlayerNetwork(pl.LightningModule):
 
         if architecture == "paper":
             self.conv1 = torch.nn.utils.weight_norm(
-                self.slayer.conv(2, 12, 5), name="weight"
+                self.slayer.conv(2, 12, 5, weight_scale=1), name="weight"
             )
             self.pool1 = self.slayer.pool(2)
             self.conv2 = torch.nn.utils.weight_norm(
-                self.slayer.conv(12, 64, 5), name="weight"
+                self.slayer.conv(12, 64, 5, weight_scale=1), name="weight"
             )
             self.pool2 = self.slayer.pool(2)
             self.fc1 = torch.nn.utils.weight_norm(
@@ -54,23 +49,27 @@ class SlayerNetwork(pl.LightningModule):
 
         elif architecture == "larger":
             self.conv1 = torch.nn.utils.weight_norm(
-                self.slayer.conv(2, 16, 3, padding=1), name="weight"
+                self.slayer.conv(2, 16, 3, weight_scale=1, padding=1), name="weight"
             )
 
             self.pool1 = self.slayer.pool(2)
             self.conv2 = torch.nn.utils.weight_norm(
-                self.slayer.conv(16, 32, 3, padding=1), name="weight"
+                self.slayer.conv(16, 32, 3, weight_scale=1, padding=1), name="weight"
             )
             self.pool2 = self.slayer.pool(2)
             self.conv3 = torch.nn.utils.weight_norm(
-                self.slayer.conv(32, 64, 3, padding=1), name="weight"
+                self.slayer.conv(32, 64, 3, weight_scale=1, padding=1), name="weight"
             )
             self.fc1 = torch.nn.utils.weight_norm(
-                self.slayer.dense((8, 8, 64), 512), name="weight"
+                self.slayer.dense((8, 8, 64), 512, weight_scale=1), name="weight"
             )
             self.fc2 = torch.nn.utils.weight_norm(
-                self.slayer.dense(512, 10), name="weight"
+                self.slayer.dense(512, 10, weight_scale=1), name="weight"
             )
+
+        # Undo slayer's scaling of pooling layer
+        self.pool1.weight.data /= self.pool1.weight.numel() * 1.1
+        self.pool2.weight.data /= self.pool2.weight.numel() * 1.1
 
     def forward(self, x):
         x = x.movedim(1, -1)
@@ -81,10 +80,11 @@ class SlayerNetwork(pl.LightningModule):
         else:
             out1 = self.pool1(self.slayer.spike(self.slayer.psp(self.conv1(x))))
             out2 = self.pool2(self.slayer.spike(self.slayer.psp(self.conv2(out1))))
-            out3 = self.conv3(out2)
-            out = self.fc2(self.fc1(out3))
+            out3 = self.slayer.spike(self.slayer.psp(self.conv3(out2)))
+            out4 = self.slayer.spike(self.slayer.psp(self.fc1(out3)))
+            out = self.fc2(self.fc1(out4))
 
-        return self.network(out.movedim(-1, 1).flatten(-3))
+        return out.movedim(-1, 1).flatten(-3)
 
     def training_step(self, batch, batch_idx):
         x, y = batch  # x is Batch, Time, Channels, Height, Width