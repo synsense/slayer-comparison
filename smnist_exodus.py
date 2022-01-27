3729912f57241c259763a49d2dc5e18d05de6177
Gregor Lenz <gregor.lenz@synsense.ai>
2022-01-27 11:29:59 +0100

    first version of SMNIST exodus

---
+++ smnist_exodus.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import pytorch_lightning as pl
+import sinabs.layers as sl
+import sinabs.activation as sa
+from typing import Dict, Any
+import sinabs.slayer.layers as ssl
+from torch.nn.utils import weight_norm
+
+
+class SinabsNetwork(pl.LightningModule):
+    def __init__(
+        self,
+        batch_size=None,
+        tau_mem=10.0,
+        spike_threshold=0.1,
+        learning_rate=1e-3,
+        weight_decay=0,
+        width_grad=1.0,
+        scale_grad=1.0,
+        init_weights_path=None,
+        encoding_dim=80,
+        hidden_dim1=128,
+        hidden_dim2=256,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        act_fn = sa.ActivationFunction(
+            spike_threshold=spike_threshold,
+            spike_fn=sa.SingleSpike,
+            reset_fn=sa.MembraneSubtract(),
+            surrogate_grad_fn=sa.SingleExponential(
+                grad_width=width_grad, grad_scale=scale_grad
+            ),
+        )
+
+        self.network = nn.Sequential(
+            nn.Linear(encoding_dim, hidden_dim1, bias=False),
+            ssl.LIF(tau_mem=tau_mem, activation_fn=act_fn),
+            nn.Linear(hidden_dim1, hidden_dim2, bias=False),
+            ssl.LIF(tau_mem=tau_mem, activation_fn=act_fn),
+            nn.Linear(hidden_dim2, 10, bias=False),
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+    def training_step(self, batch, batch_idx):
+        self.reset_states()
+        self.zero_grad()
+        x, y = batch  # x is Batch, Time, Channels, Height, Width
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat.sum(1), y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        self.zero_grad()
+        self.reset_states()
+        x, y = batch  # x is Batch, Time, Channels, Height, Width
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat.sum(1), y)
+        self.log("valid_loss", loss, prog_bar=True)
+        prediction = y_hat.sum(1).argmax(1)
+        accuracy = (prediction == y).float().sum() / len(prediction)
+        self.log("valid_acc", accuracy, prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        self.zero_grad()
+        self.reset_states()
+        x, y = batch  # x is Batch, Time, Channels, Height, Width
+        y_hat = self(x)
+        prediction = y_hat.sum(1).argmax(1)
+        accuracy = (prediction == y).float().sum() / len(prediction)
+        self.log("test_acc", accuracy)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(
+            self.parameters(),
+            lr=self.hparams.learning_rate,
+            weight_decay=self.hparams.weight_decay,
+        )
+
+    @property
+    def spiking_layers(self):
+        return [
+            layer
+            for layer in self.network.children()
+            if isinstance(layer, sl.StatefulLayer)
+        ]
+
+    @property
+    def weight_layers(self):
+        return [
+            layer
+            for layer in self.network.children()
+            if not isinstance(layer, sl.StatefulLayer)
+        ]
+
+    def zero_grad(self):
+        for layer in self.spiking_layers:
+            layer.zero_grad()
+
+    def reset_states(self):
+        for layer in self.spiking_layers:
+            layer.reset_states()
+
+    def on_save_checkpoint(self, checkpoint: Dict[str, Any]) -> None:
+        for name, parameter in checkpoint["state_dict"].items():
+            # uninitialise states so that there aren't any problems
+            # when loading the model from a checkpoint
+            if "v_mem" in name or "activations" in name:
+                checkpoint["state_dict"][name] = torch.zeros(
+                    (0), device=parameter.device
+                )
+        return super().on_save_checkpoint(checkpoint)
