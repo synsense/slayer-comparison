526df88a4a327f42b72553541d51e6c2ae8a76f4
Gregor Lenz <gregor.lenz@synsense.ai>
2022-05-18 20:19:36 +0800

    scale grad arg for HSD

--- hsd/sweep_hsd.sh
+++ hsd/sweep_hsd.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-#for tau in 30 100000;
-for rand_seed in 24 42;
+# for tau in 30 100000;
+# for rand_seed in 24 42;
+for scale in 0.01 0.1;
 do
-    python train_hsd.py --max_epochs=200 --track_grad_norm=2 --tau_mem=100000 --rand_seed=$rand_seed
+    python train_hsd.py --max_epochs=200 --track_grad_norm=2 --tau_mem=100000 --scale_grad=$scale
 done
